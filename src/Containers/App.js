808a536dd60260b2607aa9e084fadcf31f0e19d8
ishanarola <45381325+ishanarola@users.noreply.github.com>
2023-07-07 17:24:43 +0530

    useEffect added

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,38 +1,60 @@
-import React, { Component } from "react";
+import { useState,useEffect } from "react";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import Scroll from "../Components/Scroll";
 import ErrorBoundry from "../Components/ErrorBoundry";
 // import { robots } from "./robots";
 
-class App extends Component {
-  state = {
-    robots: [],
-    searchString: "",
-  };
+function App()  {
+  // state = {
+  //   robots: [],
+  //   searchString: "",
+  // };
+const [robots,setRobots]=useState([]);
+const [searchString,setSearchString]=useState("");
 
-  async componentDidMount() {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not OK");
+// async componentDidMount(){
+//   try {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/users"
+//     );
+//     if (!response.ok) {
+//       throw new Error("Network response was not OK");
+//     }
+//     const result = await response.json();
+//     this.setState({ robots: result });
+//     // .then((users) => {});
+//   } catch (error) {
+//     console.error("Error: ", error);
+//   }
+// }
+  useEffect(()=> {
+    async function fetchData(){
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
+        }
+        const result = await response.json();
+        setRobots( result);
+        // .then((users) => {});
+      } catch (error) {
+        console.error("Error: ", error);
       }
-      const result = await response.json();
-      this.setState({ robots: result });
-      // .then((users) => {});
-    } catch (error) {
-      console.error("Error: ", error);
     }
-  }
-  onSearchChange = (e) => {
-    this.setState({ searchString: e.target.value });
+    fetchData();
+  },[])
+
+
+  const onSearchChange = (e) => {
+    setSearchString( e.target.value );
   };
-  render() {
-    const { robots } = this.state;
+  
+   
     const filteredRobots = robots.filter((robot) =>
-      robot.name.toLowerCase().includes(this.state.searchString.toLowerCase())
+      robot.name.toLowerCase().includes(searchString.toLowerCase())
     );
     return !robots.length ? (
       <div className="wrapper">
@@ -41,7 +63,7 @@ class App extends Component {
     ) : (
       <div className="tc">
         <h1 className="f1">Robo Friends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         {/* If anything happens in CardLiSt, ErrorBoundry will catch it and show error */}
         <ErrorBoundry>
           <Scroll>
@@ -51,6 +73,5 @@ class App extends Component {
       </div>
     );
   }
-}
 
 export default App;
